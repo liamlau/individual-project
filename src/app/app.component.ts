5530b7615f8b06fed69293992386731604b0be84
Liam Lau <2314991l@student.gla.ac.uk>
2020-10-01 18:31:18 +0100

    Implemented generic code allowing variables to be shown in descriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,50 +11,73 @@ export class AppComponent {
   currentStep = 0;
   timeInBetween: number = 500;
   commandList = [];
+
   commandMap = {
     0: "",
-    1: "Start a loop for 2 times",
-    2: "Print 1 to console.",
-    3: "Print 2 to console.",
-    4: "Print 3 to console.",
-    5: "Print 4 to console.",
-    6: "Print 5 to console.",
-    7: "Print 6 to console.",
+    1: "Start a loop for 7 times and increment i on each loop",
+    2: "Print i (%i%) to console.",
+    3: "Done!"
   };
 
-  test() {
+  returnText = "Click start to run the program below!";
+
+  executeFunction() {
     this.commandList = [];
     this.commandList.push(1);
-    for (let i=0; i<2; i++) {
-      console.log(1);
-      this.commandList.push(2);
-      console.log(2);
-      this.commandList.push(3);
-      console.log(3);
-      this.commandList.push(4);
-      console.log(4);
-      this.commandList.push(5);
-      console.log(5);
-      this.commandList.push(6);
-      console.log(6);
-      this.commandList.push(7);
+    for (let i=1; i<8; i++) {
+      console.log(i);
+      this.commandList.push({2: {"%i%": i}});
     }
+    this.commandList.push(3);
     this.play();
     console.log(this.commandList);
   }
 
   async play() {
     for (let command of this.commandList) {
-      let a = document.getElementById("line" + command);
-      a.style.color = "#37FF00";
-      this.currentStep = command;
-      await this.sleep(this.timeInBetween);
-      a.style.color = "";
+      if (command instanceof Object) {
+        let commandNum = Number(Object.keys(command)[0]);
+
+        this.returnText = this.generateMessage(commandNum, command[Object.keys(command)[0]]);
+
+        let a = document.getElementById("line" + commandNum);
+        a.style.color = "#37FF00";
+        this.currentStep = commandNum;
+        await this.sleep(this.timeInBetween);
+        a.style.color = "";
+
+        this.returnText = this.commandMap[commandNum];
+
+      } else {
+        console.log("here");
+        let a = document.getElementById("line" + command);
+        a.style.color = "#37FF00";
+        this.currentStep = command;
+        this.returnText = this.commandMap[command];
+        await this.sleep(this.timeInBetween);
+        a.style.color = "";
+      }
     }
   }
 
   async sleep(msec: number) {
     return new Promise(resolve => setTimeout(resolve, msec));
+  }
+
+  generateMessage(commandNum: number, replacements: Object): string {
+    console.log(commandNum);
+    console.log(replacements);
+
+    var str = this.commandMap[commandNum];
+
+    // FROM: https://stackoverflow.com/questions/7975005/format-a-javascript-string-using-placeholders-and-an-object-of-substitutions
+    str = str.replace(/%\w+%/g, function(all) {
+      return replacements[all] || all;
+    });
+
+    console.log(str);
+
+    return str;
   }
 
   // slow() {
