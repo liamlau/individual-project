f3eff8614f6ba93c304b267ada1725e269d231c1
Liam Lau <2314991l@student.gla.ac.uk>
2020-11-17 12:20:50 +0000

    Added initial hiding of code block by clicking arrow header tab

--- src/app/algorithm-page/code-display/code-display.component.ts
+++ src/app/algorithm-page/code-display/code-display.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PlaybackService } from '../playback.service';
 
+declare var anime: any;
+
 @Component({
   selector: 'code-display',
   templateUrl: './code-display.component.html',
@@ -17,7 +19,42 @@ export class CodeDisplayComponent implements OnInit {
   }
 
   toggleExpansion() {
+    var terminalElement = document.getElementById("terminal");
+    if (this.animate) {
+      console.log(terminalElement.style.display);
+      terminalElement.style.display = "none";
+      // anime({
+      //   targets: '.terminal-header',
+      //   easing: 'easeInOutQuint',
+      //   translateY: [0, 200],
+      //   duration: 400
+      // })
+      anime({
+        targets: '.terminal',
+        easing: 'easeInOutQuint',
+        translateY: [0, -50],
+        opacity: [1, 0],
+        duration: 400
+      })
+    } else {
+      // maybe try animating the height?
+      terminalElement.style.display = "";
+      // anime({
+      //   targets: '.terminal-header',
+      //   easing: 'easeInOutQuint',
+      //   translateY: [200, 0],
+      //   duration: 400
+      // })
+      anime({
+        targets: '.terminal',
+        easing: 'easeInOutQuint',
+        translateY: [-20, 0],
+        opacity: [0, 1],
+        duration: 400
+      })  
+    }
     this.animate = !this.animate;
+
   }
 
 }
