56ff15a3fa7b104787e67a4fe730de24a89f47db
Liam Lau <2314991l@student.gla.ac.uk>
2021-02-06 04:09:51 +0000

    Started redesign of home page (v2)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 
 const routes: Routes = [
-  { path: '', component: AlgorithmPageComponent },
+  { path: '', component: HomePageComponent },
   { path: 'algorithm', component: AlgorithmPageComponent, canActivate: [AlgorithmGuard] },
   { path: '**', component: HomePageComponent },  // Wildcard route
 ];
