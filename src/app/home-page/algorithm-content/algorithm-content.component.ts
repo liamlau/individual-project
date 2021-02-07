067c4b7b43e5150284c3e8f3d2afd838707cbefc
Liam Lau <2314991l@student.gla.ac.uk>
2021-02-07 19:16:56 +0000

    Made Algorithms page with initial design algorithm cards

--- src/app/home-page/algorithm-content/algorithm-content.component.ts
+++ src/app/home-page/algorithm-content/algorithm-content.component.ts
@@ -4,7 +4,7 @@ import { simpleFadeAnimation } from 'src/app/animations';
 @Component({
   selector: 'app-algorithm-content',
   templateUrl: './algorithm-content.component.html',
-  styleUrls: ['./algorithm-content.component.scss'],
+  styleUrls: ['./algorithm-content.component.scss', '../home-page.component.scss', '../home-content/home-content.component.scss'],
   animations: [
     simpleFadeAnimation
   ]
