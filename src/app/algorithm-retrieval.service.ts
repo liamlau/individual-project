be6cbed864165f74efc55b2e2f9f8ce78888c054
Liam Lau <2314991l@student.gla.ac.uk>
2021-02-14 06:27:44 +0000

    Fully implemented GS Stable Marriage using new system

--- src/app/algorithm-retrieval.service.ts
+++ src/app/algorithm-retrieval.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Algorithm } from './Algorithm';
 import { EgsResidentHSService } from './algorithm-page/algorithms/egs-resident-hs/egs-resident-hs.service';
 import { GaleShapleyService } from './algorithm-page/algorithms/gale-shapley/gale-shapley.service';
+import { GsStableMarriageService } from './algorithm-page/algorithms/smp-man-gs/gs-stable-marriage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,29 +15,29 @@ export class AlgorithmRetrievalService {
   numberOfGroup2Agents: number = 5;
 
   mapOfAvailableAlgorithms: Map<String, Algorithm> = new Map([
-    // [
-    //   "smp-man-gs", {
-    //     id: "smp-man-gs",
-    //     name: "Stable Marriage Problem",
-    //     orientation: ["Man", "Woman"],
-    //     algorithm: "Gale-Shapley Stable Matching",
-    //     service: this.gsService,
-    //     description: "The stable marriage problem is the problem of finding a stable matching between two equally sized sets of elements. In this case: <b>men and women</b>.<br><br>To do this, the Gale-Shapley Stable Marriage algorithm is used.",
-    //     helpTextMap: {
-    //       1: "Set the 'match' attribute of men and women to null",
-    //       2: "While freeMen still has men in it, select the first one as m (%man%)",
-    //       3: "%woman% is selected as %man%'s most preferred woman who he has not yet proposed to",
-    //       4: "Checking to see if %woman% has a match",
-    //       5: "%woman% was free, so matching her with %man%",
-    //       6: "%woman% is currently matched to %match%, so can't instantly engage %woman% and %man%",
-    //       7: "Checking if %woman% likes %match% more than %man%",
-    //       8: "%woman% likes %man% (current proposer) more than %match% (current match) so free %match% and engage %woman% and %man%",
-    //       9: "%woman% likes %match% more than %man%",
-    //       10: "No change to anyone's matches",
-    //       11: "A stable matching has been generated."
-    //     }
-    //   }
-    // ],
+    [
+      "smp-man-gs", {
+        id: "smp-man-gs",
+        name: "Stable Marriage Problem",
+        orientation: ["Man", "Woman"],
+        algorithm: "Gale-Shapley Stable Matching",
+        service: this.gsStableMarriageService,
+        description: "The stable marriage problem is the problem of finding a stable matching between two equally sized sets of elements. In this case: <b>men and women</b>.<br><br>To do this, the Gale-Shapley Stable Marriage algorithm is used.",
+        helpTextMap: {
+          1: "Set the 'match' attribute of men and women to null",
+          2: "While freeMen still has men in it, select the first one as m (%man%)",
+          3: "%woman% is selected as %man%'s most preferred woman who he has not yet proposed to",
+          4: "Checking to see if %woman% has a match",
+          5: "%woman% was free, so matching her with %man%",
+          6: "%woman% is currently matched to %match%, so can't instantly engage %woman% and %man%",
+          7: "Checking if %woman% likes %match% more than %man%",
+          8: "%woman% likes %man% (current proposer) more than %match% (current match) so free %match% and engage %woman% and %man%",
+          9: "%woman% likes %match% more than %man%",
+          10: "No change to anyone's matches",
+          11: "A stable matching has been generated."
+        }
+      }
+    ],
 
     [
       "smp-man-egs", {
@@ -104,7 +105,7 @@ export class AlgorithmRetrievalService {
   // }
 
   constructor(
-    public gsService: GaleShapleyService,
+    public gsStableMarriageService: GsStableMarriageService,
     public egsResidentHsService: EgsResidentHSService,
   ) { }
 
