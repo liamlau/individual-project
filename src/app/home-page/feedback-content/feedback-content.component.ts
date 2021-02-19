5dff227801350830e9c86cf0e2423be3cfc26ecf
Liam Lau <2314991l@student.gla.ac.uk>
2021-02-19 19:06:44 +0000

    Added some content for feedback tab

--- src/app/home-page/feedback-content/feedback-content.component.ts
+++ src/app/home-page/feedback-content/feedback-content.component.ts
@@ -4,7 +4,7 @@ import { simpleFadeAnimation } from 'src/app/animations/fadeAnimation';
 @Component({
   selector: 'app-feedback-content',
   templateUrl: './feedback-content.component.html',
-  styleUrls: ['./feedback-content.component.scss'],
+  styleUrls: ['./feedback-content.component.scss', '../home-page.component.scss', '../home-content/home-content.component.scss'],
   animations: [
     simpleFadeAnimation
   ]
