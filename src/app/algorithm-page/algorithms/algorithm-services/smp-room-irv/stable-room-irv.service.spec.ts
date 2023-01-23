60f3eeb5976aaf9fe2b91be52ff1b9d699f9f4af
CallumOrmond <callumormond10@gmail.com>
2023-01-23 14:31:28 +0000

    Remove SR Tests - SR no dynamic pref generation

--- src/app/algorithm-page/algorithms/algorithm-services/smp-room-irv/stable-room-irv.service.spec.ts
+++ src/app/algorithm-page/algorithms/algorithm-services/smp-room-irv/stable-room-irv.service.spec.ts
@@ -17,19 +17,19 @@ describe('StableRoomIrvService', () => {
   // added test from smp
 
   // tests dont work cause there is no pref generation - fixed at 6 - this tried to make different sizes 
-  it("test correctness x 10 (smp-room-irv)", () => {
-    let stable: boolean = true;
-    for (let i = 0; i < 10; i++) {
-      let agentCount: number = Math.floor(Math.random() * (9 - 2) + 2);
-      service.run(agentCount, agentCount, undefined);
-      if (!service.stable) {
-        stable = false;
-      }
-      console.log(service.stable)
+  // it("test correctness x 10 (smp-room-irv)", () => {
+  //   let stable: boolean = true;
+  //   for (let i = 0; i < 10; i++) {
+  //     let agentCount: number = Math.floor(Math.random() * (9 - 2) + 2);
+  //     service.run(agentCount, agentCount, undefined);
+  //     if (!service.stable) {
+  //       stable = false;
+  //     }
+  //     console.log(service.stable)
 
-    }
+  //   }
 
-    expect(stable).toBeTrue();
-  })
+  //   expect(stable).toBeTrue();
+  // })
 
 });
